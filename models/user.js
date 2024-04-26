103d4c42497a25e17dc3831d4844478bbfb9da01
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-04-25 20:12:07 -0500

    created userSchema

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "User must have a name"]
+    },
+    password: {
+        type: String,
+        required: [true, "User must have a password"]
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ["Admin, Author, User"]
+    }
+})
+
+module.exports = mongoose.model("User", userSchema)
