426c3c4a361ea6674458aeb33ca264cd4ab874a8
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-04-25 19:56:49 -0500

    created postSchema

---
+++ models/post.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({ 
+    title: {
+        type: String,
+        required: [true, "Post must have a title"]
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    text: {
+        type: String,
+        required: [true, "Post must have text"]
+    },
+    user: {
+        type: Schema.Types.ObjectId, ref: "User", required: true
+    },
+    visibility: {
+        type: String,
+        enum: ["Published", "Not Published"],
+        default: "Not Published",
+        required: true;
+    }
+})
+
+postSchema.virtual("date_formatted").get(function() {
+    return DateTime.fromJSDate(this.date).toLocaleString(DateTime.DATE_MED);
+})
+
+module.exports = mongoose.model("Post", postSchema);
