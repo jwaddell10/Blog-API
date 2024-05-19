bf0724d02895523958ff8045735f8565cf0bfd94
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-05-19 12:23:13 -0500

    update for question

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,60 +23,51 @@ exports.signupPost = [
 
 	// console.log(req.body.formDataObject, "this is reqbody");
 	// //take request body, let usersign in, sanitize password?
-	asyncHandler(async (req, res, next) => {
-		console.log(req.body.formDataObject.password, "this is password");
-		const name = req.body.formDataObject.name;
-		try {
-			const duplicate = await User.findOne({ name: name });
-			if (duplicate) {
-				res.status(400).json({ errorMessage: "user already exists" });
-			} else {
-				next();
-			}
-		} catch (error) {
-			next(error);
-		}
-	}),
 
 	body("name", "You must enter a username")
 		.trim()
 		.notEmpty()
 		.isAlphanumeric()
+		.custom(async (req) => {
+			console.log(req, "this is req");
+			const name = req.body.name;
+			const existingUser = await User.findOne(name);
+			console.log(existingUser, "this is existinguser");
+			if (existingUser) {
+				throw new Error("User already exists");
+			}
+		})
 		.withMessage("Username can only contain letters and numbers")
 		.escape(),
-	body("password", "You must enter a password")
-		.trim()
-		.notEmpty()
-		.matches("passwordConfirmation")
-		.escape(),
+	body("password", "You must enter a password").trim().notEmpty().escape(),
 	body("passwordConfirmation", "You must enter a password")
-		.matches("password")
-		// .custom((value) => {
-		// 	if (value !== req.body.formDataObject.password) {
-		// 		throw new Error("Passwords do not match");
-		// 	}
-		// })
+		.custom((value) => {
+			console.log(value, 'this is value')
+			if (value !== req.body.formDataObject.password) {
+				throw new Error("Passwords do not match");
+			}
+		})
 		.escape(),
 
-	asyncHandler(async (req, res, next) => {
-		const errors = validationResult(req);
+	// asyncHandler(async (req, res, next) => {
+	// 	const errors = validationResult(req);
 
-		const name = req.body.formDataObject.name;
-		try {
-			bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
-				if (err) {
-					throw new Error("User could not be created");
-				}
-				const user = new User({
-					name: name,
-					password: hashedPassword,
-				});
+	// 	const name = req.body.formDataObject.name;
+	// 	try {
+	// 		bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+	// 			if (err) {
+	// 				throw new Error("User could not be created");
+	// 			}
+	// 			const user = new User({
+	// 				name: name,
+	// 				password: hashedPassword,
+	// 			});
 
-				await user.save();
-				res.redirect("/");
-			});
-		} catch (error) {
-			next(error);
-		}
-	}),
+	// 			await user.save();
+	// 			res.redirect("/");
+	// 		});
+	// 	} catch (error) {
+	// 		next(error);
+	// 	}
+	// }),
 ];
